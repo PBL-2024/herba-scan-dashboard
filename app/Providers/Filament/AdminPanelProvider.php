f5211bea1fdda062ee93dc27c08f555f5c860733
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2024-10-21 21:15:45 +0700

    Update AdminPanelProvider: use asset() for brand logo and favicon paths

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,9 +30,9 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Green,
             ])
-            ->brandLogo('images/logo.png')
+            ->brandLogo(asset('images/logo.png'))
             ->brandLogoHeight('3rem')
-            ->favicon('images/logo.png')
+            ->favicon(asset('images/logo.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
