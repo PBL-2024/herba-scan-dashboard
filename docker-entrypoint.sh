c864e1fd2a34e18b5c9f6c506fafa43b05df14c7
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2025-11-24 15:42:55 +0700

    update docker compose

--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Fix permissions for storage and bootstrap/cache
-chmod -R 777 /var/www/storage /var/www/bootstrap/cache 2>/dev/null || true
-
-# Execute the main command directly
-exec "$@"
